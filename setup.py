925042f48d1cde2164d8e1cde4688286cb0c66bb
thgribeiro <thiagordlr@gmail.com>
2022-10-25 18:38:56 -0300

    já tinha bin

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ import os
 
 options = webdriver.ChromeOptions()
 options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
